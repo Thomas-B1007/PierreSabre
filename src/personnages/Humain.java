c49c8182fa92b8ccad04f7e943c85cc92bb90047
Thomas-B1007 <thomas.bjn65@gmail.com>
2025-11-17 16:28:30 +0100

    creation de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,55 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFavorite;
+	private int argent;
+	
+	public Humain(String nom, String boissonFavorite, int argent) {
+		super();
+		this.nom = nom;
+		this.boissonFavorite = boissonFavorite;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("(" + nom + ") - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFavorite + ".");
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
+	}
+	
+	private void gagnerArgent(int montant) {
+		argent += montant;
+	}
+	
+	private void perdreArgent(int montant) {
+		if (argent > montant) {
+			argent -= montant;
+		} else {
+			argent = 0;
+		}
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (argent >= prix) {
+			 parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+			 perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+		}
+	}
+}
